67d5f34925c34f87a3b982c4e1b21b0d6d2af49a
Evgeniy Drachev <edrachev@gmail.com>
2016-04-20 13:03:16 +0300

    session bean

--- src/main/java/leftpad/controllers/ApplicationController.java
+++ src/main/java/leftpad/controllers/ApplicationController.java
@@ -7,21 +7,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import leftpad.beans.LastPadded;
 import leftpad.forms.PadderForm;
 import leftpad.services.PadderService;
 
 @Controller
 public class ApplicationController {
 	private final PadderService padderService;
+	private final LastPadded lastPadded;
 
 	@Autowired
-	public ApplicationController(PadderService padderService) {
+	public ApplicationController(PadderService padderService, LastPadded lastPadded) {
 		this.padderService = padderService;
+		this.lastPadded = lastPadded;
 	}
 
 	@RequestMapping("")
 	public String home(Model model) {
 		PadderForm form = new PadderForm();
+		form.setInput(lastPadded.getValue());
 		model.addAttribute("padderForm", form);
 		return "main";
 	}
@@ -30,6 +34,7 @@ public class ApplicationController {
 	public CompletionStage<String> doPadding(PadderForm form) {
 		String input = form.getInput();
 		return padderService.padAsync(input).thenApply(output -> {
+			lastPadded.setValue(input);
 			form.setOutput(output);
 			return "main";
 		});
