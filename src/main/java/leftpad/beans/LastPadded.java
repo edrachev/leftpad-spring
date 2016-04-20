97e7ea2e4934a6caaa3247d07a5d48e987bf063a
Evgeniy Drachev <edrachev@gmail.com>
2016-04-20 13:03:16 +0300

    session bean

---
+++ src/main/java/leftpad/beans/LastPadded.java
@@ -0,0 +1,19 @@
+package leftpad.beans;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class LastPadded {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
--- src/main/java/leftpad/controllers/ApplicationController.java
+++ src/main/java/leftpad/controllers/ApplicationController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import leftpad.beans.LastPadded;
 import leftpad.forms.PadderForm;
 import leftpad.services.PadderService;
 
@@ -12,22 +13,27 @@ import leftpad.services.PadderService;
 @Controller
 public class ApplicationController {
     private final PadderService padderService;
+    private final LastPadded lastPadded;
     
     @Autowired
-    public ApplicationController(PadderService padderService) {
+    public ApplicationController(PadderService padderService, LastPadded lastPadded) {
 	this.padderService = padderService;
+	this.lastPadded = lastPadded;
     }
 
     @RequestMapping("")
     public String home(Model model) {
 	PadderForm form = new PadderForm();
+	form.setInput(lastPadded.getValue());
 	model.addAttribute("padderForm", form);
 	return "main";
     }
     
     @RequestMapping("/padding")
     public String doPadding(PadderForm form) {
-	form.setOutput(padderService.pad(form.getInput()));
+	String input = form.getInput();
+	lastPadded.setValue(input);
+	form.setOutput(padderService.pad(input));
 	return "main";
     }
 }
