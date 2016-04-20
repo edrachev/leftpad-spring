e6935e323df45b612cc0a3f0ac393371c288efd7
Evgeniy Drachev <edrachev@gmail.com>
2016-04-20 12:58:20 +0300

    Service and DI

---
+++ src/main/java/leftpad/services/PadderService.java
@@ -0,0 +1,11 @@
+package leftpad.services;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PadderService {
+	public String pad(String input) {
+		return StringUtils.leftPad(input, 10, "*");
+	}
+}
