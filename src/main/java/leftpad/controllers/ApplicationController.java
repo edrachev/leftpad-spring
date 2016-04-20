1e4ae7622595c92532b6a5d31baf8f7ef815244d
Evgeniy Drachev <edrachev@gmail.com>
2016-04-20 12:58:20 +0300

    Service and DI

--- src/main/java/leftpad/controllers/ApplicationController.java
+++ src/main/java/leftpad/controllers/ApplicationController.java
@@ -1,13 +1,22 @@
 package leftpad.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import leftpad.forms.PadderForm;
+import leftpad.services.PadderService;
 
 @Controller
 public class ApplicationController {
+	private final PadderService padderService;
+
+	@Autowired
+	public ApplicationController(PadderService padderService) {
+		this.padderService = padderService;
+	}
+
 	@RequestMapping("")
 	public String home(Model model) {
 		PadderForm form = new PadderForm();
@@ -17,7 +26,7 @@ public class ApplicationController {
 
 	@RequestMapping("/padding")
 	public String doPadding(PadderForm form) {
-		form.setOutput("Ooops!");
+		form.setOutput(padderService.pad(form.getInput()));
 		return "main";
 	}
 }
