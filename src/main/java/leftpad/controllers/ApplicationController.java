960106352e135fdc12c7e842862b48f460845594
Evgeniy Drachev <edrachev@gmail.com>
2016-04-20 12:46:33 +0300

    forms and reverse routing

--- src/main/java/leftpad/controllers/ApplicationController.java
+++ src/main/java/leftpad/controllers/ApplicationController.java
@@ -1,12 +1,23 @@
 package leftpad.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import leftpad.forms.PadderForm;
 
 @Controller
 public class ApplicationController {
-    @RequestMapping("")
-    public String home() {
-	return "main";
-    }
+	@RequestMapping("")
+	public String home(Model model) {
+		PadderForm form = new PadderForm();
+		model.addAttribute("padderForm", form);
+		return "main";
+	}
+
+	@RequestMapping("/padding")
+	public String doPadding(PadderForm form) {
+		form.setOutput("Ooops!");
+		return "main";
+	}
 }
